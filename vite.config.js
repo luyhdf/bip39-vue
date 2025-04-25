e366479732b3e780da36f35939961de7c5d32501
luyhdf <461587276@qq.com>
2025-04-25 08:30:55 +0800

    更新 Vite 配置，修改基础路径并调整 HTML 文件中的资源链接

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/bip39-vue/",
   plugins: [vue()],
   build: {
-      outDir: "docs",
-      base: "/bip39-vue/"
+      outDir: "docs"
     }
 })
